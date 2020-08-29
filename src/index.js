6eae8dc03d4d5178416084bd3a99763a00af427d
jehernandezv <jhonn.hernandez@uptc.edu.co>
2020-08-29 17:25:32 -0500

    update

--- src/index.js
+++ src/index.js
@@ -24,28 +24,51 @@ app.get('/berkeley', function (req, res) {
 
 //iniciar servidor
 const server = app.listen(port, () => {
-  console.log('listening on *:3000');
+  console.log('listening on port: ' + port);
 });
 
 const io = SocketIO(server);
 
 //websockets
 io.on('connection', (socket) => {
-  console.log('new connection',socket.id);
-  socket.on('hour:client',(data) => {
-      const offset = getoffset(data);
-      io.emit('res:time',{
-        offset:'ajuste'
-      });
+  console.log('new connection', socket.id);
+  socket.on('hour:client', async (data) => {
+    const offset = await getoffset(data);
+    console.log(offset);
+    io.emit('res:time', {
+      offset: offset
+    });
   });
 });
+
+
 
 //llamado de la hora a cada cierto tiempo
 setInterval(function () {
   io.emit('req:time');
 }, 3000);
 
-function getoffset(timeClient){
-  console.log(timeClient);
+async function getoffset(timeClient) {
+ var offset = 0;
+  axios.get('http://worldtimeapi.org/api/timezone/America/Bogota').then(async (response) => {
+    const timeApi = await response.data.unixtime;
+    dateObj = new Date(timeApi * 1000);
+    // Get hours from the timestamp 
+    hours = (dateObj.getUTCHours() - 5);
+    // Get minutes part from the timestamp 
+    minutes = dateObj.getUTCMinutes();
+    // Get seconds part from the timestamp 
+    seconds = dateObj.getUTCSeconds();
+    let auxSecond = 0;
+    auxSecond += hours*3600;
+    auxSecond+= minutes*60;
+    auxSecond+= seconds;
+    offset = auxSecond - timeClient.timeClient;
+    console.log('method: ' + offset);
+    return offset;
+  }).catch((error) => {
+    console.log(error);
+    return 0;
+  });
 }
 
