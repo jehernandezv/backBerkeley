c37b22c7361c095d47140a5e07e07e43a4e27694
jehernandezv <jhonn.hernandez@uptc.edu.co>
2020-08-29 16:25:15 -0500

    backend con socket io

--- src/index.js
+++ src/index.js
@@ -4,37 +4,48 @@ const app = express();
 const cors = require('cors');
 const axios = require('axios');
 const body_parser = require('body-parser');
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const SocketIO = require('socket.io');
 
 //inicializaciones
 const port = process.argv[2];
 
 //middleware
 app.use(morgan('dev'));
-app.use(body_parser.urlencoded({extended:false}));
+app.use(body_parser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
 //Routes
-app.get('/berkeley',function(req,res){
-    res.json({
-        message:'get'
-    });
+app.get('/berkeley', function (req, res) {
+  res.json({
+    message: 'get'
+  });
 });
 
-//recibe conexiones
-io.of('clients').on('connection', (socket) => {
-    console.log('A new user connected');
-  });
-
-  setTimeout(function() {
-    var event = setInterval(function () {
-      io.of('clients').emit('time', new Date());
-    }, 1000);
-  }, 5000);
-
 //iniciar servidor
-http.listen(port, () => {
-    console.log('listening on *:3000');
+const server = app.listen(port, () => {
+  console.log('listening on *:3000');
+});
+
+const io = SocketIO(server);
+
+//websockets
+io.on('connection', (socket) => {
+  console.log('new connection',socket.id);
+  socket.on('hour:client',(data) => {
+      const offset = getoffset(data);
+      io.emit('res:time',{
+        offset:'ajuste'
+      });
   });
+});
+
+//llamado de la hora a cada cierto tiempo
+setInterval(function () {
+  io.emit('req:time');
+}, 3000);
+
+function getoffset(timeClient){
+  console.log(timeClient);
+}
+
